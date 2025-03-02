ffcfe3875809fb1de36a4f344fb38debb19aa8a8
Priyanka Jaiswal <prijais@microsoft.com>
2025-03-02 03:02:38 -0800

    updating the file and endpoint

--- webportal/src/App.js
+++ webportal/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Stack } from '@fluentui/react';
+import { incident } from './incident';
+import React from 'react';
+
+export const iframeId = 'copilotforservice-ppe';
 
 function App() {
+  const [selected, setSelected] = React.useState(null);
+  const case1 = incident({ index: 0, selected, onSelected: () => setSelected(0) });
+  const case2 = incident({ index: 1, selected,  onSelected: () => setSelected(1) });
+  const case3 = incident({ index: 2, selected, onSelected: () => setSelected(2) });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <Stack horizontal>
+          <Stack.Item>
+            <iframe id={iframeId} src="https://copilotforservice-ppe.azureedge.net/widget/index.html?dynamicsUrl=https://orge0da95d1.crm.dynamics.com" frameborder="0" style={{
+              height: '100vh',
+              width: '50vw'
+            }} />
+          </Stack.Item>
+          <Stack.Item style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch',
+          }}>
+            {case1}
+            {case2}
+            {case3}
+          </Stack.Item>
+        </Stack>
+      </body>
     </div>
   );
 }
