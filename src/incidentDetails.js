2b5f7b6398b71aaadef149b009cd4f457ca7529f
Priyanka Jaiswal <prijais@microsoft.com>
2025-03-02 03:10:44 -0800

    updating the file and endpoint

---
+++ src/incidentDetails.js
@@ -0,0 +1,62 @@
+export const incidentsData = [
+  {
+    "id": "1",
+    "title": "Incident 1",
+    "description": "This is the first incident",
+    "status": "Active"
+  },
+  {
+    "id": "2",
+    "title": "Incident 2",
+    "description": "This is the second incident",
+    "status": "Active"
+  },
+  {
+    "id": "3",
+    "title": "Incident 3",
+    "description": "This is the third incident",
+    "status": "Active"
+  },
+  {
+    "id": "4",
+    "title": "Incident 4",
+    "description": "This is the fourth incident",
+    "status": "Active"
+  },
+  {
+    "id": "5",
+    "title": "Incident 5",
+    "description": "This is the fifth incident",
+    "status": "Active"
+  },
+  {
+    "id": "6",
+    "title": "Incident 6",
+    "description": "This is the sixth incident",
+    "status": "Active"
+  },
+  {
+    "id": "7",
+    "title": "Incident 7",
+    "description": "This is the seventh incident",
+    "status": "Active"
+  },
+  {
+    "id": "8",
+    "title": "Incident 8",
+    "description": "This is the eighth incident",
+    "status": "Active"
+  },
+  {
+    "id": "9",
+    "title": "Incident 9",
+    "description": "This is the ninth incident",
+    "status": "Active"
+  },
+  {
+    "id": "10",
+    "title": "Incident 10",
+    "description": "This is the tenth incident",
+    "status": "Active"
+  }
+];
